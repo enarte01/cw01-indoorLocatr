5a8058e4bd78218d23cb7f2f677b12e106ccfe2e
enarte01 <enarte01@mail.bbk.ac.uk>
2017-03-25 14:06:53 +0000

    first commit

---
+++ app/src/main/java/com/mciot/cw/enarte01/indoorlocatr/IndoorLocatrActivity.java
@@ -0,0 +1,13 @@
+package com.mciot.cw.enarte01.indoorlocatr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndoorLocatrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indoor_locatr);
+    }
+}
